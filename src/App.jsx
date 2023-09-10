426bb9c822993ac42e5e4a7d50e1e0dbd3fbee0c
TevenV27 <victoria.kevin@correounivalle.edu.co>
2023-09-09 21:50:22 -0500

    ADD Mobile Nav

--- src/App.jsx
+++ src/App.jsx
@@ -19,50 +19,61 @@ function App() {
                 <button className='b-close-session'> Cerrar Sesión</button>
             </section>
 
+
             <section className='section-section-artist'>
+                <section className='navigation-box-mobile'>
+                    <div>
+                        <h1 className='name-app'><span style={{ color: "#00C2FF" }}>We</span>Tunes</h1>
+                    </div>
+                    <button className='b-menu'>
+                        <span class="material-symbols-outlined menu-icon">
+                            menu
+                        </span>
+                    </button>
+                </section>
                 <div className='container-song-artist'>
-                    
-                        <div className='songs-box'>
-                            {/* Pasamos la función setSelectedSong como prop */}
-                            <Songs onSongClick={song => setSelectedSong({
-                                songName: song.nombre,
-                                songArtist: song.cantante,
-                                songImage: song.imagen,
-                                audioPath: song.cancion,
-                            })} />
+
+                    <div className='songs-box'>
+                        {/* Pasamos la función setSelectedSong como prop */}
+                        <Songs onSongClick={song => setSelectedSong({
+                            songName: song.nombre,
+                            songArtist: song.cantante,
+                            songImage: song.imagen,
+                            audioPath: song.cancion,
+                        })} />
+                    </div>
+
+                    <div className='rigth-panel-box'>
+                        <div className='user-info-box'>
+                            <span><span style={{ color: "#00C2FF" }}>Steven</span> Victoria</span>
+                            <img className='avatar-user' src="https://pbs.twimg.com/profile_images/1471595683660587010/p-wPFxIp_400x400.jpg" alt="" />
                         </div>
-
-                        <div className='rigth-panel-box'>
-                            <div className='user-info-box'>
-                                <span><span style={{ color: "#00C2FF" }}>Steven</span> Victoria</span>
-                                <img className='avatar-user' src="https://pbs.twimg.com/profile_images/1471595683660587010/p-wPFxIp_400x400.jpg" alt="" />
-                            </div>
-                            <div className='artist-box'>
-                                <h2 className='title-artist'>ARTISTAS</h2>
-                                <Artist />
-                            </div>
+                        <div className='artist-box'>
+                            <h2 className='title-artist'>ARTISTAS</h2>
+                            <Artist />
                         </div>
-                 
+                    </div>
 
 
-                    
+
+
 
                 </div>
 
                 <div className='song-control'>
-                        {/* Renderizamos Reproductor solo si hay una canción seleccionada */}
-                        {selectedSong && (
-                            <Reproductor
-                                songName={selectedSong.songName}
+                    {/* Renderizamos Reproductor solo si hay una canción seleccionada */}
+                    {selectedSong && (
+                        <Reproductor
+                            songName={selectedSong.songName}
 
-                                songArtist={selectedSong.songArtist}
-                                songImage={selectedSong.songImage}
-                                audioPath={selectedSong.audioPath}
+                            songArtist={selectedSong.songArtist}
+                            songImage={selectedSong.songImage}
+                            audioPath={selectedSong.audioPath}
 
-                            />
+                        />
 
-                        )}
-                    </div>
+                    )}
+                </div>
 
             </section>
         </main>
