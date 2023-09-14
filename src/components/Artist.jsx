b41f0a74b93d314f3767440593242a35c3327fd0
TevenV27 <victoria.kevin@correounivalle.edu.co>
2023-09-14 18:51:54 -0500

    ADD Search Music and Artists

--- src/components/Artist.jsx
+++ src/components/Artist.jsx
@@ -1,23 +1,39 @@
-import React , {useState, useEffect}from 'react'
-import artist from '../data/artistas.json'
-import '../stylesheet/artist-style.css'
+import React, { useState, useEffect } from 'react';
+import artist from '../data/artistas.json';
+import '../stylesheet/artist-style.css';
+
 export default function Artist() {
-    const [artistData, setArtistData] = useState([]);
+  const [artistData, setArtistData] = useState([]);
+  const [searchText, setSearchText] = useState('');
 
-    useEffect(() => {
-        setArtistData(artist);
-    }, []);
+  useEffect(() => {
+    setArtistData(artist);
+  }, []);
 
-    return (
-        <div className='artist-container' >
+  const handleSearchInputChange = (event) => {
+    setSearchText(event.target.value);
+  };
 
-            {artistData.map((artist, artistIndex) => (
-                <div className='artist-card' key={artistIndex}>
-                    <img className='img-artist' src={artist.imagen} alt="artist-image" />
-                    <p className='name-artist'> {artist.nombre}</p>
-                </div>
-            ))}
+  // Filtrar la lista de artistas en función del valor de búsqueda
+  const filteredArtists = artistData.filter((artist) =>
+    artist.nombre.toLowerCase().includes(searchText.toLowerCase())
+  );
 
+  return (
+    <div className='artist-container'>
+      <input
+        className='input-name-artist'
+        type="text"
+        placeholder='Nombre'
+        value={searchText}
+        onChange={handleSearchInputChange}
+      />
+      {filteredArtists.map((artist, artistIndex) => (
+        <div className='artist-card' key={artistIndex}>
+          <img className='img-artist' src={artist.imagen} alt="artist-image" />
+          <p className='name-artist'> {artist.nombre}</p>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
--- src/components/Songs.jsx
+++ src/components/Songs.jsx
@@ -3,60 +3,80 @@ import songs from '../data/Canciones.json';
 import '../stylesheet/songs-style.css';
 
 export default function Songs(props) {
-    const [songsData, setSongsData] = useState([]);
+  const [songsData, setSongsData] = useState([]);
+  const [searchText, setSearchText] = useState('');
 
-    useEffect(() => {
-        setSongsData(songs);
-    }, []);
+  useEffect(() => {
+    setSongsData(songs);
+  }, []);
 
-    // Función para truncar el nombre de la canción o el cantante
-    function truncateString(str, num) {
-        if (str.length <= num) {
-            return str;
-        }
-        return str.slice(0, num) + '...';
+  // Función para truncar el nombre de la canción o el cantante
+  function truncateString(str, num) {
+    if (str.length <= num) {
+      return str;
     }
+    return str.slice(0, num) + '...';
+  }
 
-    // Función que se llamará al hacer click en una canción
-    function handleSongClick(song) {
-        props.onSongClick(song); // Estamos llamando a una función que se pasó como prop desde el componente App
-    }
+  // Función que se llamará al hacer clic en una canción
+  function handleSongClick(song) {
+    props.onSongClick(song); // Estamos llamando a una función que se pasó como prop desde el componente App
+  }
 
-    // Extraer una lista única de géneros
-    const genres = [...new Set(songsData.map(song => song.genero))];
+  // Filtrar la lista de canciones en función del valor de búsqueda
+  const filteredSongs = searchText
+    ? songsData.filter((song) =>
+        song.nombre.toLowerCase().includes(searchText.toLowerCase())
+      )
+    : songsData;
 
-    return (
-        <div className='songs-container'>
-            {genres.map((genre, genreIndex) => (
-                <article className='song-box' key={genreIndex}>
-                    <div className='genre-box'>
-                        <h2 className='genre-name'>{genre.charAt(0).toUpperCase() + genre.slice(1)}</h2>
+  // Extraer una lista única de géneros basada en las canciones filtradas
+  const filteredGenres = [...new Set(filteredSongs.map((song) => song.genero))];
+
+  return (
+    <div className='songs-container'>
+      <input
+        className='input-song-name'
+        type="text"
+        placeholder='Nombre de Canción'
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+      />
+      {filteredGenres.map((genre, genreIndex) => (
+        <article className='song-box' key={genreIndex}>
+          <div className='genre-box'>
+            <h2 className='genre-name'>{genre.charAt(0).toUpperCase() + genre.slice(1)}</h2>
+          </div>
+          <div className='section-song'>
+            {filteredSongs
+              .filter((song) => song.genero === genre)
+              .map((song, songIndex) => (
+                <div className='card-song' key={songIndex}>
+                  <div className='img-song-box'>
+                    <img className='img-song' src={song.imagen} alt="song-image" />
+                    <span onClick={() => handleSongClick(song)} className="material-icons play-song-icon">
+                      play_circle
+                    </span>
+                  </div>
+                  <div className='description-song'>
+                    <div className='song-info'>
+                      <p className='song-name'>{truncateString(song.nombre, 15)}</p>
+                      <p className='artist-name'>
+                        {truncateString(
+                          Array.isArray(song.cantante) ? song.cantante.join(', ') : song.cantante,
+                          20
+                        )}
+                      </p>
                     </div>
-                    <div className='section-song'>
-                        {songsData.filter(song => song.genero === genre).map((song, songIndex) => (
-                            <div className='card-song' key={songIndex}>
-                                <div className='img-song-box'>
-                                    <img className='img-song' src={song.imagen} alt="song-image" />
-                                    <span onClick={() => handleSongClick(song)} className="material-icons play-song-icon">
-                                        play_circle
-                                    </span>
-                                </div>
-                                <div className='description-song'>
-                                    <div className='song-info'>
-                                        <p className='song-name'>{truncateString(song.nombre, 15)}</p>
-                                        <p className='artist-name'>
-                                            {truncateString(Array.isArray(song.cantante) ? song.cantante.join(', ') : song.cantante, 20)}
-                                        </p>
-                                    </div>
-                                    <span className="material-symbols-outlined">
-                                        favorite
-                                    </span>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </article>
-            ))}
-        </div>
-    )
+                    <span className="material-symbols-outlined">
+                      favorite
+                    </span>
+                  </div>
+                </div>
+              ))}
+          </div>
+        </article>
+      ))}
+    </div>
+  );
 }
